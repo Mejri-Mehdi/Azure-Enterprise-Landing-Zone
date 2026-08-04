c16fba2bb84acf7c260d442244bd66ddebc7488b
Mehdi Mejri <mehdimejri15@gmail.com>
2026-08-04 18:22:41 +0100

    feat: deploy Azure Firewall with forced tunneling UDR

--- src/networking/hub/deployment-commands.ps1
+++ src/networking/hub/deployment-commands.ps1
@@ -61,36 +61,6 @@ az network vnet subnet update `
 
 
 
----------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-az group create --name rg-networking-spoke-prod --location eastus
-az group create --name rg-networking-spoke-dev --location eastus
-
-
-
-az deployment group create `
-  --resource-group rg-networking-spoke-prod `
-  --template-file src/networking/spokes/spoke-vnet.bicep `
-  --parameters spokeName=prod addressPrefix='10.1.0.0/16'
-
-
-
-az deployment group create `
-  --resource-group rg-networking-spoke-dev `
-  --template-file src/networking/spokes/spoke-vnet.bicep `
-  --parameters spokeName=dev addressPrefix='10.2.0.0/16'
-
-
-
-
----------------------------------------------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------------------------------------------
-
-
 
 
 
